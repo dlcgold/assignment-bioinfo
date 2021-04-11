d689adfde17ffd874ed30eed11d1840d5fc971f8
dlcgold <dlcgold96@gmail.com>
2021-04-11 12:09:21 +0200

    fix del fix dopo mail

--- 1/esbio/target/debug/deps/esbio-7551c39e35ef9afb.d
+++ 1/esbio/target/debug/deps/esbio-7551c39e35ef9afb.d
@@ -1,5 +1,5 @@
-/home/dlcgold/uni/esbio/target/debug/deps/esbio-7551c39e35ef9afb: src/lib.rs
+/home/dlcgold/es_bio/1/esbio/target/debug/deps/esbio-7551c39e35ef9afb: src/lib.rs
 
-/home/dlcgold/uni/esbio/target/debug/deps/esbio-7551c39e35ef9afb.d: src/lib.rs
+/home/dlcgold/es_bio/1/esbio/target/debug/deps/esbio-7551c39e35ef9afb.d: src/lib.rs
 
 src/lib.rs:
